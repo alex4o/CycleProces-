f5131f9bf36aeb295fed0d93a4a3a2f361a0b462
Alex4o <mrabonin@gmail.com>
2014-01-30 20:48:04 +0200

    All done for big pattern. Working on Database Plugins

---
+++ Plugin/Interface.cs
@@ -0,0 +1,17 @@
+﻿using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin
+{
+
+
+    public interface IDBManager
+    {
+		void Save(IEnumerable<strtime> list);
+
+    }
+}
